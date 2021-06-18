95c9b677f5b1fdf131d7860b573e564011cb23f1
Veljko Prokic <veljkoprokic@gmail.com>
2021-06-18 22:24:56 +0200

    Basic Page

---
+++ src/Pages/BasicPage.java
@@ -0,0 +1,29 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public abstract class BasicPage {
+
+	protected WebDriver driver;
+	protected JavascriptExecutor js;
+	
+	public BasicPage(WebDriver driver, JavascriptExecutor js) {
+		this.driver = driver;
+		this.js = js;
+	}
+	
+	public boolean elementExist(By by) {
+		boolean exist = true;
+		try {
+			driver.findElement(by);
+		} catch (Exception e) {
+			exist = false;
+		}
+		return exist;
+	}
+	
+	
+	
+}
