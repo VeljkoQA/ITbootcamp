381888c6a43c4e7cc6318322bc5d5cbb5390d6c1
Veljko Prokic <veljkoprokic@gmail.com>
2021-07-01 20:23:43 +0200

    Profile Page

--- src/Pages/ProfilePage.java
+++ src/Pages/ProfilePage.java
@@ -59,4 +59,47 @@ public class ProfilePage extends BasicPage{
 		return this.driver.findElement(By.xpath("//input[@name='btn_submit']"));
 	}
 	
+	public WebElement getUpload() {
+		return this.driver.findElement(By.xpath("//*[@class='upload uploadFile-Js']"));
+	}
+	
+	public WebElement getUploadForm() {
+		return this.driver.findElement(By.xpath("//*[@id='form-upload']/input"));
+	}
+	
+	public void imgUpload(String imgPath) {
+		js.executeScript("arguments[0].click();", this.getUpload());
+		this.getUploadForm().sendKeys(imgPath);
+	}
+	
+	public WebElement getRemoveImgButton() {
+		return this.driver.findElement(By.className("remove"));
+	}
+	
+	public void deleteImg() {
+		js.executeScript("arguments[0].click();", this.getRemoveImgButton());
+	}
+	
+	public void changeInfo(String firstName, String lastName, String address, String phoneNumber, String zipCode, String country, String state, String city) 
+						  throws InterruptedException {
+		
+		this.getFirstName().clear();
+		this.getFirstName().sendKeys(firstName);
+		this.getLastName().clear();
+		this.getLastName().sendKeys(lastName);
+		this.getAddress().clear();
+		this.getAddress().sendKeys(address);
+		this.getPhoneNumber().clear();
+		this.getPhoneNumber().sendKeys(phoneNumber);
+		this.getZipCode().clear();
+		this.getZipCode().sendKeys(zipCode);
+		this.getCountry().selectByVisibleText(country);
+		Thread.sleep(2000);
+		this.getState().selectByVisibleText(state);
+		Thread.sleep(2000);
+		this.getCity().selectByVisibleText(city);
+		Thread.sleep(3000);
+		this.getSaveButton().submit();
+		
+	}
 }
